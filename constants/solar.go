b4088c5f7c7c5378d01663ac622a2618868b3734
ltfred <ltfred@163.com>
2025-02-08 14:56:02 +0800

    feat: 添加二十四节气常量

---
+++ constants/solar.go
@@ -0,0 +1,30 @@
+package constants
+
+type SolarTerm string
+
+const (
+	TheBeginningOfSpring  SolarTerm = "立春"
+	RainWater             SolarTerm = "雨水"
+	TheWakingOfInsects    SolarTerm = "惊蛰"
+	TheSpringEquinox      SolarTerm = "春分"
+	PureBrightness        SolarTerm = "清明"
+	GrainRain             SolarTerm = "谷雨"
+	TheBeginningOfSummer  SolarTerm = "立夏"
+	LesserFullnessOfGrain SolarTerm = "小满"
+	GrainInBeard          SolarTerm = "芒种"
+	TheSummerSolstice     SolarTerm = "夏至"
+	LesserHeat            SolarTerm = "小暑"
+	GreaterHeat           SolarTerm = "大暑"
+	TheBeginningOfAutumn  SolarTerm = "立秋"
+	TheEndOfHeat          SolarTerm = "处暑"
+	WhiteDew              SolarTerm = "白露"
+	TheAutumnEquinox      SolarTerm = "秋分"
+	ColdDew               SolarTerm = "寒露"
+	FrostDescent          SolarTerm = "霜降"
+	TheBeginningOfWinter  SolarTerm = "立冬"
+	LesserSnow            SolarTerm = "小雪"
+	GreaterSnow           SolarTerm = "大雪"
+	TheWinterSolstice     SolarTerm = "冬至"
+	LesserCold            SolarTerm = "小寒"
+	GreaterCold           SolarTerm = "大寒"
+)
